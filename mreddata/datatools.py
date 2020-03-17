252a9243bdefe41abfe52d474fe76967be804d2a
Matthew Breeding <matthew.l.breeding@vanderbilt.edu>
2020-03-17 00:35:19 -0500

    Added nIons, sortOrder in combine method

--- mreddata/datatools.py
+++ mreddata/datatools.py
@@ -286,7 +286,7 @@ class HistogramList:
          
 
 
-	def combineHistograms(self, newHistName, label="", color="", histograms=[], filename = 'combined histograms'):
+	def combineHistograms(self, newHistName, label="", color="", histograms=[], filename = 'combined histograms', sortOrder = None, nIons=0):
 		#TODO: add method for saving to file, extend the custoHistograms list
 		''' Combines all of the histgorams in the current Histogram object list''' 
 		if not histograms:
@@ -305,10 +305,13 @@ class HistogramList:
 		else:
 			print("ERROR: No histograms passed to combineHistograms()")
 			return False
-		nTotal = sum([h.nIons for h in histograms])
+		if nIons == 0:
+			nTotal = sum([h.nIons for h in histograms])
+		else:
+			nTotal = nIons
 		cDF = histograms[0].df.loc[:,('x', 'w')]
 		cDF[['y','y2','n']] = sum([h.df[['y_raw','y2_raw', 'n']] for h in histograms])
-		newHist = Histogram(histname = newHistName , filename=filename, label=label, color=color, df = cDF, gfu = gfu, nIons=nTotal)
+		newHist = Histogram(histname = newHistName , filename=filename, label=label, color=color, df = cDF, gfu = gfu, nIons=nTotal, sortOrder = sortOrder)
 		self.customHistograms.append(newHist)
 		return newHist
 
